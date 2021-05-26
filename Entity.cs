68e1ba44cab9a180d764f6701d6cc32ed9e6abb4
Evgh <kasperovich.official@gmail.com>
2021-05-26 18:07:09 +0300

    Добавьте файлы проекта.

---
+++ Entity.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Entity;
+using System.Collections.Generic;
+
+
+namespace s4_oop_l11
+{
+    public class Soda
+    {
+        public int SodaId { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+
+
+        public Company Company { get; set; }
+        public int CompanyId { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    } 
+
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+
+        public ICollection<Soda> Sodas { get; set; }
+        public Company()
+        {
+            Sodas = new List<Soda>();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class SodaContext : DbContext
+    {
+        public SodaContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Soda> Sodas { get; set; }
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace s4_oop_l11
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SodaContext sodaDataBase;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            sodaDataBase = new SodaContext();
+
+            sodaDataBase.Sodas.Load();
+            sodaDataBase.Companies.Load();
+
+            sodaGrid.ItemsSource = sodaDataBase.Sodas.Local.ToBindingList();
+            companiesGrid.ItemsSource = sodaDataBase.Companies.Local.ToBindingList();
+            CompaniexComboBoxColumn.ItemsSource = sodaDataBase.Companies.Local.ToBindingList();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sodaDataBase.SaveChanges();
+        }
+
+    
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            int caloriesValue;
+            if (int.TryParse(caloriesTexBox.Text, out caloriesValue))
+            {
+                var sodas = from soda in sodaDataBase.Sodas
+                            where soda.Calories < caloriesValue
+                            where soda.Name == nameTextBox.Text
+                            select soda;
+
+
+                 searchGrid.ItemsSource = sodas.ToList();
+            }
+
+        }
+    }
+}
